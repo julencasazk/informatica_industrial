2c20953ed14804d2353ee11eb0dd9989a92abe7b
julencasazk <julen.casal@opendeusto.es>
2023-03-01 17:32:51 +0100

    txeinyes

--- Actividad_Operadores_Bits/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main(int argc, char const *argv[])
-{
-    unsigned short bruh = 0;
-    printf("%d\n", sizeof(bruh));
-    return 0;
-}
---
+++ Mem_Dinamica/ips.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct Dispositivo
+{
+    unsigned short id;
+    unsigned char ip[15+1];
+} Dispositivo;
+
+typedef struct MES
+{
+    unsigned char mesIp[15+1];
+    unsigned short numDisp;
+    Dispositivo *devices;
+} MES;
+
+MES leeFichero(char *nombreFichero){
+
+    FILE *fichero = fopen(nombreFichero, "r");
+    assert(fichero != NULL && "Error al abrir fichero");
+
+    MES mes;
+
+    char primero[15+1];
+    short segundo;
+
+    int scan = fscanf(fichero, " IP_MES: %s N: %hd", mes.mesIp, &mes.numDisp);
+    mes.devices = (Dispositivo*)malloc(mes.numDisp * sizeof(Dispositivo));
+
+    for (size_t i = 0; i < mes.numDisp; i++)
+    {
+        scan = fscanf(fichero, "%hd: %s", &mes.devices[i].id, mes.devices[i].ip);
+    }
+    
+    return mes;
+}
+
+
+int main()
+{
+    MES mes;
+    mes = leeFichero("DatosConfig.txt");
+
+    printf("Ordenador donde se sitúa el programa MES: %s\n\n", mes.mesIp);
+
+    for (int i = 0; i < mes.numDisp; i++){
+        printf("Codigo del PLC(%d): %d, direccion IP: %s\n", i, mes.devices[i].id, mes.devices[i].ip);
+    }
+
+    return 0;
+}
+
+
