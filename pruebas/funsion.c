dd945acce99faf1eaf4a648722b8705c7c5b7b0b
julencasazk <julen.casal@opendeusto.es>
2023-03-20 08:00:22 +0100

    funcion a aprenderse para los examenes

--- pruebas/funsion.c
+++ pruebas/funsion.c
@@ -13,13 +13,35 @@ void desplazarBitsIzquierda(char* bloqueActual, int tamanyoBloque, char* bloqueN
         }
         
     }
-    
     // Acordarze de asignar memoria a bloqueNuevo con malloc, y liberarla cuando no la necesitas.
 
 }
 
 int main()
 {
+
+    FILE *fichero = fopen("codigoBarras.txt", "a"); // Hacer esto al principio de cada lectura
+    // Convertir 
+    unsigned long ms = 7200000;
+
+    unsigned long seg;
+    unsigned long min;
+    unsigned long hora;
+    unsigned long dia;
+
+    seg = ms / 1000;
+    ms %= 1000;
+    min = seg / 60;
+    seg %= 60;
+    hora = min / 60;
+    min %= 60;
+    dia = hora / 24;
+    hora %= 24;
+
+    printf("[%d;%d:%d:%d,%d]\n", dia, hora, min, seg, ms);
+
+
+    fclose(fichero);
 
     return 0;
 }
